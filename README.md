0fc754e4edb24ee16d8ed0cd3e960f4f4e3626ed
Ashwin <ashwin031192@gmail.com>
2021-04-11 20:30:57 +0530

    Minor tweaks and updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-WikiTree
+# WikiTree
+A visual attempt to verify the [theory](https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy) that clicking on the first link of any given wikipedia article, and repeating the process for subsequent articles, usually leads to the [Philosophy](https://en.wikipedia.org/wiki/Philosophy) article
+
+## Issues
+CSS Selector is not quite there yet leading to selection of the wrong link as the "first" link in an article
--- wikitreeapi/wiki.js
+++ wikitreeapi/wiki.js
@@ -4,14 +4,34 @@ import cheerio from 'cheerio';
 class Wiki {
     constructor() {
         this.base_url = 'https://en.wikipedia.org/wiki/';
+        const base = '.mw-parser-output a[href][title]:first-of-type';
+        const conditions = [
+            ':not(.image)',
+            ':not(.internal)',
+            ':not(.hatnote a)',
+            ':not(table a)',
+            ':not(sup a)',
+            ':not(small a)',
+            ':not(.tright a)',
+            ':not(.rt-commentedText a)',
+            ':not(.rt-commentedText+a)',
+            ':not(.IPA a)',
+            ':not(.haudio a)',
+            ':not(.zh-Hans a)',
+            ':not(.nowraplinks a)',
+            ':not(#coordinates a)',
+            ':not(#right-navigation a)',
+            ':not(.mw-editsection a)'
+        ];
+
+        this.selector = base + conditions.join('');
     }
 
     async parse(slug) {
         const url = this.base_url + slug;
 
         const html = await requestPromise(url);
-        const cssPath = '.mw-parser-output a[href]:not(.image):not(.internal):first-of-type:not(.hatnote a):not(table a):not(sup a):not(.tright a):not(.rt-commentedText a):not(.rt-commentedText+a):not(.IPA a):not(.haudio a):not(small a):not(.zh-Hans a):not(.nowraplinks a)';
-        const parsed = cheerio(cssPath, html);
+        const parsed = cheerio(this.selector, html);
 
         const keys = Object.keys(parsed);
         const elem = parsed[keys[0]];
